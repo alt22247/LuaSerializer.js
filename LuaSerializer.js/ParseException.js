2acda9a9e36784a4fd923c9fefb209c6b4ad2937
Steven Chen <alt22247@hotmail.com>
2019-09-07 19:24:06 -0400

    Revert "initial commit"
    This reverts commit 5023e9bd695373dca49235ac9e228fb1c920f7d2.

--- LuaSerializer.js/ParseException.js
+++
@@ -1,7 +0,0 @@
-class ParseException extends Error {
-    constructor(msg) {
-        super(msg);
-        this.type = "ParseException";
-    }
-}
-//# sourceMappingURL=ParseException.js.map
--- LuaSerializer.js/Scripts/UnitTest/LuaFiles/TypeTestDictionary.js
+++
@@ -1,6 +0,0 @@
-LuaFiles.add("DictionaryBasic", `{
-	[0] = 0,
-	[1] = 1,
-	[2] = 2
-}`);
-//# sourceMappingURL=TypeTestDictionary.js.map
