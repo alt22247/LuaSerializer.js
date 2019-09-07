69f96335dd5d645409b6c4e9a97c650d1b53a85e
Steven Chen <alt22247@hotmail.com>
2019-09-07 19:27:53 -0400

    remove compiled files

--- LuaSerializer.js/LuaSerializer.js
+++
@@ -1,9 +0,0 @@
-class LuaSerializer {
-    deserialize(data) {
-        if (data == null)
-            return null;
-        let parser = new DataParser(data);
-        return parser.parse();
-    }
-}
-//# sourceMappingURL=LuaSerializer.js.map
